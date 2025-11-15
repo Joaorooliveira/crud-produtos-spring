e7d35a96eb560591ae4f3ef4049f1cb424a70083
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-11-15 15:53:47 -0300

    feat: adiciona classe para tratar as excessoes globais

--- src/main/java/com/product/api/crud_produtos/infra/TratadorDeErro.java
+++ src/main/java/com/product/api/crud_produtos/infra/TratadorDeErro.java
@@ -3,18 +3,25 @@ package com.product.api.crud_produtos.infra;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-public class TratadorDeErro extends ResponseEntityExceptionHandler {
+public class TratadorDeErro {
 
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity tratarErro404(EntityNotFoundException ex) {
         var erro  = new MensagemErro(ex.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
     }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarErro400(MethodArgumentNotValidException ex) {
+        var erros  = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).toList());
+    }
+
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity tratarErro500(Exception ex) {
@@ -24,5 +31,12 @@ public class TratadorDeErro extends ResponseEntityExceptionHandler {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro: " + ex.getLocalizedMessage());
     }
 
+    private record DadosErroValidacao(String campo, String mensagem){
+
+        public DadosErroValidacao(FieldError erro){
+            this(erro.getField(), erro.getDefaultMessage());
+        }
+
+    }
     private record MensagemErro(String mensagem) {}
 }
