bc6c956782ba7ab260e5cbd79089f3324cb6a3ec
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-11-15 23:31:03 -0300

    feat: Adiciona nova entidade Usuario

---
+++ src/main/java/com/product/api/crud_produtos/entity/Usuario.java
@@ -0,0 +1,28 @@
+package com.product.api.crud_produtos.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity(name = "Usuario")
+@Table(name = "usuarios")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+
+public class Usuario {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String login;
+
+    @Column(nullable = false)
+    private String senha;
+
+}
