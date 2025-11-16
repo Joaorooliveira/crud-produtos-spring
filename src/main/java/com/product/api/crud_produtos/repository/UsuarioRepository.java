e9b8f87de3151bd082078c4ed933625d92c6bfba
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-11-16 00:00:43 -0300

    feat(usuario-repository): adiciona metodo pra buscar usuario por login no banco de dados

--- src/main/java/com/product/api/crud_produtos/repository/UsuarioRepository.java
+++ src/main/java/com/product/api/crud_produtos/repository/UsuarioRepository.java
@@ -2,6 +2,8 @@ package com.product.api.crud_produtos.repository;
 
 import com.product.api.crud_produtos.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    UserDetails findByLogin(String login);
 }
---
+++ src/main/java/com/product/api/crud_produtos/service/AutenticacaoService.java
@@ -0,0 +1,4 @@
+package com.product.api.crud_produtos.service;
+
+public class AutenticacaoService {
+}
