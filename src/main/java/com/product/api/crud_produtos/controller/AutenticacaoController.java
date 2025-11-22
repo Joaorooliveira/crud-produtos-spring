788722d058d4b4100aa654dfb907fa493c0cb3f3
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-11-21 22:24:27 -0300

    refactor(autenticacao-controller): Atualiza nome das rotas

--- src/main/java/com/product/api/crud_produtos/controller/AutenticacaoController.java
+++ src/main/java/com/product/api/crud_produtos/controller/AutenticacaoController.java
@@ -1,6 +1,9 @@
 package com.product.api.crud_produtos.controller;
 
-import com.product.api.crud_produtos.dto.DadosAutenticacao;
+import com.product.api.crud_produtos.infra.security.DadosAutenticacao;
+import com.product.api.crud_produtos.entity.Usuario;
+import com.product.api.crud_produtos.infra.security.DadosTokenJWT;
+import com.product.api.crud_produtos.infra.security.TokenService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,21 +14,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/login")
+@RequestMapping("api/auth")
 public class AutenticacaoController {
 
     private final AuthenticationManager manager;
+    private final TokenService tokenService;
 
-    public AutenticacaoController(AuthenticationManager manager) {
+    public AutenticacaoController(AuthenticationManager manager, TokenService tokenService) {
         this.manager = manager;
+        this.tokenService = tokenService;
     }
 
-    @PostMapping
-    public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados){
-        var token = new UsernamePasswordAuthenticationToken(dados.login(),dados.senha());
-        var authentication = manager.authenticate(token);
 
-        return ResponseEntity.ok().build();
+
+    @PostMapping("/login")
+    public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados){
+        var authenticationToken = new UsernamePasswordAuthenticationToken(dados.login(),dados.senha());
+        var authentication = manager.authenticate(authenticationToken);
+
+        var tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
+        return ResponseEntity.ok(new DadosTokenJWT(tokenJWT));
     }
 
 }
