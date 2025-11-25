9a7c3a1b3068976ac64c154554cec5d81fbf065d
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-11-24 23:20:50 -0300

    feat(usuario-service): adiciona metodo atualizar usuario por id

--- src/main/java/com/product/api/crud_produtos/service/UsuarioService.java
+++ src/main/java/com/product/api/crud_produtos/service/UsuarioService.java
@@ -1,10 +1,14 @@
 package com.product.api.crud_produtos.service;
 
+import com.product.api.crud_produtos.dto.request.UsuarioAtualizarRequestDTO;
+import com.product.api.crud_produtos.dto.response.ProdutoResponseDTO;
 import com.product.api.crud_produtos.dto.response.UsuarioResponseDTO;
 import com.product.api.crud_produtos.entity.Usuario;
 import com.product.api.crud_produtos.repository.UsuarioRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -27,6 +31,19 @@ public class UsuarioService {
 
     public Optional<Usuario> buscarUsuarioPorId (Long id) {
         return repository.findById(id);
+    }
+
+    public UsuarioResponseDTO atualizarUsuarioPorId  (Long id, UsuarioAtualizarRequestDTO dto ) {
+        var UsuarioEntity = buscarUsuarioPorId(id).orElseThrow(()-> new EntityNotFoundException("Usuario nao encontrado " +
+                "com o ID:"+id));
+
+        var usuario = Usuario.builder()
+                .id(UsuarioEntity.getId())
+                .login(dto.login()!=null?dto.login():UsuarioEntity.getLogin())
+                .senha(dto.senha()!=null?dto.senha():UsuarioEntity.getSenha())
+                .build();
+        repository.saveAndFlush(usuario);
+        return UsuarioResponseDTO.fromEntity(usuario);
     }
 
 }
