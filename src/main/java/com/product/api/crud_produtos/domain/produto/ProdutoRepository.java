b517e9696fed18e90614b7b881a5c7436016bd1b
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-12-02 15:21:50 -0300

    feat(produto-repository): adiciona metodo para buscar produtos por categoria no banco de dados

--- src/main/java/com/product/api/crud_produtos/domain/produto/ProdutoRepository.java
+++ src/main/java/com/product/api/crud_produtos/domain/produto/ProdutoRepository.java
@@ -1,9 +1,13 @@
 package com.product.api.crud_produtos.domain.produto;
 
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface ProdutoRepository extends JpaRepository<Produto, UUID> {
+
+    Page<Produto> findByCategoriaId(UUID categoriaId, Pageable pageable);
 }
