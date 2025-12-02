a19f667ad9a65e23f5463f57a1b7daa234c4c3d1
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-12-02 16:24:10 -0300

    feat: adiciona nova exception validacao exception

--- src/main/java/com/product/api/crud_produtos/infra/exception/ValidacaoException.java
+++ src/main/java/com/product/api/crud_produtos/infra/exception/ValidacaoException.java
@@ -1,4 +1,7 @@
 package com.product.api.crud_produtos.infra.exception;
 
-public class ValidacaoException {
+public class ValidacaoException extends RuntimeException {
+    public ValidacaoException(String mensagem) {
+        super(mensagem);
+    }
 }
