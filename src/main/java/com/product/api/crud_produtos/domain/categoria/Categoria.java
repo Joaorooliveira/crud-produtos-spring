cf51c8dd1f7402427078ccea32e2fd4c39c42ac3
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-12-02 12:11:34 -0300

    feat: adiciona entidade Categoria

--- src/main/java/com/product/api/crud_produtos/domain/categoria/Categoria.java
+++ src/main/java/com/product/api/crud_produtos/domain/categoria/Categoria.java
@@ -1,4 +1,26 @@
 package com.product.api.crud_produtos.domain.categoria;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "categorias")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "nome", nullable = false, unique = true)
+    private String nome;
+
 }
