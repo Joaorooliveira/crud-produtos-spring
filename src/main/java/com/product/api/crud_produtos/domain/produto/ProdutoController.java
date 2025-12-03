08901bd5ec90aa6e95169dae458857f994402e89
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-12-03 17:32:18 -0300

    docs: adiciona anotacao de documentacao para: - ProdutoController - AutenticacaoController - UsuarioController

--- src/main/java/com/product/api/crud_produtos/domain/produto/ProdutoController.java
+++ src/main/java/com/product/api/crud_produtos/domain/produto/ProdutoController.java
@@ -3,6 +3,9 @@ package com.product.api.crud_produtos.domain.produto;
 import com.product.api.crud_produtos.domain.produto.dto.ProdutoAtualizarRequestDTO;
 import com.product.api.crud_produtos.domain.produto.dto.ProdutoRequestDTO;
 import com.product.api.crud_produtos.domain.produto.dto.ProdutoResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -17,12 +20,15 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/api/produtos")
 @RequiredArgsConstructor
+@SecurityRequirement(name = "bearer-key")
+@Tag(name = "Produtos", description = "Gerenciador de Produtos")
 public class ProdutoController {
 
 
     private final ProdutoService service;
 
     @PostMapping
+    @Operation(summary = "Cadastrar Produto", description = "Cadastra um novo Produto e retorna os dados criados")
     public ResponseEntity<ProdutoResponseDTO> criarProduto(@RequestBody @Valid ProdutoRequestDTO dto,
                                                            UriComponentsBuilder builder) {
         var produto = service.criarProduto(dto);
@@ -34,6 +40,7 @@ public class ProdutoController {
     }
 
     @GetMapping
+    @Operation(summary = "Listar Produto", description = "Lista todos os produtos ou produtos por categoria")
     public ResponseEntity<Page<ProdutoResponseDTO>> listarProdutos(
             @PageableDefault(size = 10) Pageable pageable,
             @RequestParam(required = false) UUID categoriaId) {
@@ -41,6 +48,7 @@ public class ProdutoController {
     }
 
     @GetMapping("{id}")
+    @Operation(summary = "Buscar Produto pelo ID", description = "Busca produto especifico informando o ID")
     public ResponseEntity<ProdutoResponseDTO> buscarPorId(@PathVariable UUID id) {
         return service.buscarPorId(id)
                 .map(produto -> ResponseEntity.ok(ProdutoResponseDTO.fromEntity(produto))) // converte entity para DTO
@@ -48,12 +56,15 @@ public class ProdutoController {
     }
 
     @PatchMapping("{id}")
+    @Operation(summary = "Atualizar dados de Produto pelo ID", description = "Atualiza dados de um Produto" +
+            " informado pelo ID ")
     public ResponseEntity<ProdutoResponseDTO> atualizarProduto(@PathVariable UUID id, @RequestBody
     ProdutoAtualizarRequestDTO dto) {
         return ResponseEntity.ok(service.atualizarProduto(id, dto));
     }
 
     @DeleteMapping("{id}")
+    @Operation(summary = "Deletar Produto", description = "Deleta um Produto especifico informando o ID")
     public ResponseEntity<Void> deletarProduto(@PathVariable UUID id) {
         service.deletarProduto(id);
         return ResponseEntity.noContent().build();
--- src/main/java/com/product/api/crud_produtos/domain/usuario/UsuarioController.java
+++ src/main/java/com/product/api/crud_produtos/domain/usuario/UsuarioController.java
@@ -2,6 +2,9 @@ package com.product.api.crud_produtos.domain.usuario;
 
 import com.product.api.crud_produtos.domain.usuario.dto.UsuarioAtualizarRequestDTO;
 import com.product.api.crud_produtos.domain.usuario.dto.UsuarioResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
@@ -10,32 +13,39 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/usuarios")
+@SecurityRequirement(name = "bearer-key")
+@Tag(name = "Usuarios", description = "Gerenciador de Usuarios")
 public class UsuarioController {
 
     private final UsuarioService service;
+
     public UsuarioController(UsuarioService service) {
         this.service = service;
     }
 
     @GetMapping
-    public Page<UsuarioResponseDTO> listarUsuarios (Pageable pageable) {
+    @Operation(summary = "Listar Usuarios", description = "Lista todos os usuarios")
+    public Page<UsuarioResponseDTO> listarUsuarios(Pageable pageable) {
         return service.listarUsuarios(pageable);
     }
 
     @GetMapping("{id}")
-    public ResponseEntity<UsuarioResponseDTO> buscarUsuarioPorId (@PathVariable Long id) {
+    @Operation(summary = "Listar Usuario", description = "Lista Usuario especifico pelo ID")
+    public ResponseEntity<UsuarioResponseDTO> buscarUsuarioPorId(@PathVariable Long id) {
         return service.buscarUsuarioPorId(id)
                 .map(usuario -> ResponseEntity.ok(UsuarioResponseDTO.fromEntity(usuario)))
                 .orElse(ResponseEntity.notFound().build());
     }
 
     @PatchMapping("{id}")
-    public ResponseEntity<UsuarioResponseDTO> atualizarUsuarioPorId (@PathVariable Long id,
-                                                                     @RequestBody UsuarioAtualizarRequestDTO dto) {
+    @Operation(summary = "Atualizar Usuario", description = "Atualiza Usuario especifico pelo ID")
+    public ResponseEntity<UsuarioResponseDTO> atualizarUsuarioPorId(@PathVariable Long id,
+                                                                    @RequestBody UsuarioAtualizarRequestDTO dto) {
         return ResponseEntity.ok(service.atualizarUsuarioPorId(id, dto));
     }
 
     @DeleteMapping("{id}")
+    @Operation(summary = "Excluir Usuario", description = "Excluir Usuario especifico pelo ID informado")
     public ResponseEntity excluirUsuarioPorId(@PathVariable Long id) {
         service.excluirUsuarioPorID(id);
         return ResponseEntity.noContent().build();
