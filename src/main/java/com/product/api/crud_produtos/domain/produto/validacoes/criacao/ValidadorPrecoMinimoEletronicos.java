506e6a61e72edbe1f4874605c44d88c57a1adde6
Joaorooliveira <joaovictorooliveira812@gmail.com>
2025-12-03 00:03:54 -0300

    feat: adiciona validador de preco minimo para eletronicos

--- src/main/java/com/product/api/crud_produtos/domain/produto/validacoes/criacao/ValidadorPrecoMinimoEletronicos.java
+++ src/main/java/com/product/api/crud_produtos/domain/produto/validacoes/criacao/ValidadorPrecoMinimoEletronicos.java
@@ -1,4 +1,4 @@
-package com.product.api.crud_produtos.domain.produto.validacoes;
+package com.product.api.crud_produtos.domain.produto.validacoes.criacao;
 
 import com.product.api.crud_produtos.domain.categoria.CategoriaRepository;
 import com.product.api.crud_produtos.domain.produto.dto.ProdutoRequestDTO;
